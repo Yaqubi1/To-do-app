3f40b7cd30f3516bcb7ecae5e9106297d134f037
Yaqubi1 <Yaqubi1_1@users.noreply.github.com>
2020-08-12 22:23:38 +0430

    can now remove the row when clicked on Färdig

--- js/script.js
+++ js/script.js
@@ -1,4 +1,9 @@
 let toDoClickCounter = 0;
+let FinishedClickCounter = 0;
+let finishedButtonCounter = 0;
+let ChangeButtonCounter = 0;
+let finishedButton = [];
+let changeButton = [];
 
 document.getElementById("addTodo").addEventListener("click", function addToDoList() {
     let x = document.getElementById("syssla").value;
@@ -30,27 +35,68 @@ function addListItemforToDoList() {
     let toDoListItem = document.createElement("li");
     toDoListItem.setAttribute("class", "toDoListItem");
     document.getElementById("toDoList").appendChild(toDoListItem);
-        
+
     let toDoListInput = document.createElement("input");
     toDoListInput.setAttribute("type", "text");
-    
+
     let syssla = document.getElementById("syssla");
     toDoListInput.setAttribute("placeholder", syssla.value);
-    
+
     toDoListInput.setAttribute("disabled", true)
     toDoListItem.appendChild(toDoListInput);
 
     let toDoListChangeButton = document.createElement("button");
     toDoListChangeButton.innerHTML = "Ändra";
+    toDoListChangeButton.setAttribute("id", "changeButtonid" + ChangeButtonCounter);
+    ChangeButtonCounter++;
     toDoListItem.appendChild(toDoListChangeButton);
 
     let toDoListFinishedButton = document.createElement("button");
     toDoListFinishedButton.innerHTML = "Färdig";
-    toDoListInput.setAttribute("id", "changeButton");
+    toDoListFinishedButton.setAttribute("id", "finishedButton" + finishedButtonCounter);
+    toDoListFinishedButton.setAttribute("onclick", "myFunction(event)");
+    finishedButtonCounter++;
     toDoListItem.appendChild(toDoListFinishedButton);
 
 }
 
-function resetForm() {
-    document.getElementById("myForm").reset();
+function myFunction(event) {
+    let x = event.target.getAttribute("id");
+
+    if (document.getElementById(x).parentElement.parentElement.children[0].innerHTML.substr(1, 5) === "input") {
+        document.getElementById(x).parentElement.remove();
+    } else {
+        document.getElementById(x).parentElement.parentElement.remove();
+        document.getElementById(x).parentElement.parentElement.previousSibling.remove(); // remove h1 Att göra
+    }
+    
+
+    let finishedTitle = document.createElement("h1");
+            finishedTitle.innerHTML = "Färdiga";
+            finishedTitle.setAttribute("id", "finishedTitle");
+            document.body.appendChild(finishedTitle);
 }
+
+// for (let i = 0; i <= finishedButtonCounter; i++) {
+//     document.getElementById(""+x).addEventListener("click", function addToFinishedList() {
+//         if (finishedButtonCounter == 1) {
+            
+
+//             let finishedList = document.createElement("ul");
+//             finishedList.setAttribute("id", "finishedList");
+//             document.body.appendChild(finishedList);
+
+//             document.getElementById(finishedButtonCounter + i).previousElementSibling.remove();
+
+//         } else {
+//             document.getElementById(finishedButtonCounter + i).previousElementSibling.remove();
+//             resetForm();
+//         }
+//     });
+// }
+
+
+
+// function resetForm() {
+//     document.getElementById("myForm").reset();
+// }
